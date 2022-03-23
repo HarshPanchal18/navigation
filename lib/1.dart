69ede85aa33acf7e3f37e0f10420ce9e8639a791
Harsh Panchal <harshhhh1803@gmail.com>
2022-03-23 18:20:58 +0530

    New Project

---
+++ lib/1.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class TextDemo extends StatelessWidget {
+  const TextDemo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Text Demo"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Text Demo"),
+            SizedBox(height: 50.0,),
+            RaisedButton(onPressed: (){
+              Navigator.pop(context);
+             /* Navigator.push(context,
+                  MaterialPageRoute(
+                     builder: (builder) => ButtonDemo()
+                  )
+              );*/
+            },
+              child: Text("Button Demo"),
+            ),
+            SizedBox(height: 20.0,),
+            RaisedButton(
+                onPressed: () => {
+                  Navigator.pop(context) // redirect to back page
+                },
+                color: Colors.red,
+                child: Text("back")
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:demo/1.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+          Text('Hello',
+          style: TextStyle(
+            fontSize: 20.2,
+            backgroundColor: Colors.yellow,
+          ),
+          ),
+          ],
+        ),
+      ),
+
+      drawer: Drawer(
+        child: Column(
+          children: [
+            const UserAccountsDrawerHeader(
+                accountName: Text('harshiv'),
+                accountEmail: Text('harshu@gmail.com'),
+                currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.purple,
+                child: Text('rana'),
+              ),
+            ),
+
+              Divider(height: 0.8),
+
+            ListTile(
+              title: Text('cricket'),
+              leading: Icon(Icons.account_balance_wallet_rounded),
+              onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (builder) => TextDemo()));
+              },
+            )
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
